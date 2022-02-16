23847e927d928860eb332c244426585f8ae6b453
Manuel Assad <manuelnasif@gmail.com>
2022-02-15 23:02:04 -0800

    baseUrl

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,8 +1,8 @@
 // export const baseUrl = "https://json-server-cookbook1.glitch.me/";
 
-// export const baseUrl = "https://cookbookserver.com/";
+export const baseUrl = "https://cookbookserver.com/";
 
-export const baseUrl = "http://127.0.0.1:3000/";
+// export const baseUrl = "http://127.0.0.1:3000/";
 
 export const baseUrlcdn =
   "https://cdn.glitch.me/fa975902-7a10-4922-b93a-5444668498f7%2F";
